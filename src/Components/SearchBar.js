8111ba05770b8cb3201a7b3213b8c2877ab3eee7
SeaWaves12 <seagupta12@gmail.com>
2021-03-11 22:19:45 +0530

    fixed search bar, fetched data and display it

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -4,48 +4,54 @@ import Posts from './Posts';
 import SearchBar from "material-ui-search-bar";
 
 
-
+const baseURI = 'http://newsapi.org/v2';
 
 export default function PrimarySearchAppBar() {
   const [dataState, setdataState] = React.useState([]);
   const [valueState, setValueState] = React.useState('')
 
-  const fetchData = () => {
-    fetch('http://newsapi.org/v2/top-headlines?country=in&apiKey=6a1d4f2afd3b4379bd04f56e72a8bb6d')
+  const searchDataHandler = () => {
+    const reqUri = `${baseURI}/everything?q=${valueState}&from=2021-03-01&sortBy=publishedAt&apiKey=6a1d4f2afd3b4379bd04f56e72a8bb6d`
+    fetch(reqUri)
       .then(response => { 
         return response.json()
       }).then(data => {
-        return (
-          setdataState(data.articles)
-          // console.log("Data: ", data)
-              );
+        console.log("Data: ", data);
+        return setdataState(data.articles)
       })
   }
 
-    const mystyle = {
-      color: "Blue",
-      backgroundColor: "LightBlue",
-      padding: "10px",
-      fontFamily: "Arial",
-      
-    };
+  const mystyle = {
+    color: "Blue",
+    backgroundColor: "LightBlue",
+    padding: "10px",
+    fontFamily: "Arial",
+    
+  };
 
 
   const articles = dataState.map(article =>{
-    return <Posts key={article.title} title={article.title}></Posts>
+    return (
+      <Posts 
+        key={`${article.author}${article.url}${article.urlToImage}`} 
+        title={article.title}>
+      </Posts>
+    )
+  })
+
+  const cancelSearchHandler = () => {
+    // setValueState('');
+    setdataState([]);
   }
-
-  )
-
 
 
   return (
     <div style={mystyle} >
       <SearchBar
-        onCancelSearch={() => setValueState('')}
+        onCancelSearch={cancelSearchHandler}
         value={valueState}
         onChange={(e) => setValueState(e)}
-        onRequestSearch={fetchData} 
+        onRequestSearch={searchDataHandler} 
       />
       {articles}
     </div>
